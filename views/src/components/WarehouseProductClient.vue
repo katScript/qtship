ae351e468d7dd6924a60f5d1c5ec6306f10ad581
duongph <chieuduong1102@gmai.com>
2022-11-04 13:51:08 +0700

    QT-40 Validate Form create warehouse and product

--- views/src/components/WarehouseProductClient.vue
+++ views/src/components/WarehouseProductClient.vue
@@ -242,7 +242,8 @@
                           </div>
                           <div class="col-8">
                             <div class="form-group">
-                              <label for="">Ảnh sản phẩm</label> <span>{{formDataProduct.image}}</span>
+                              <label for="">Ảnh sản phẩm</label>
+                              <span>{{ formDataProduct.image }}</span>
                               <input
                                 type="file"
                                 class="form-control"
@@ -292,15 +293,15 @@
                           class=""
                           v-if="urlImgProductUpload && !formDataProduct.image"
                           :src="urlImgProductUpload"
-                        />                        
+                        />
                         <img
-                        alt=""
-                        width="200"
-                        height="200"
-                        class=""
-                        v-if="formDataProduct.image"
-                        :src="formDataProduct.image"
-                      />
+                          alt=""
+                          width="200"
+                          height="200"
+                          class=""
+                          v-if="formDataProduct.image"
+                          :src="formDataProduct.image"
+                        />
                       </div>
                     </div>
                   </div>
@@ -363,10 +364,7 @@
                     />
                   </template>
                   <template #item-image="item">
-                    <img
-                      :src="item.image"
-                      style="width: 80px; height: 80px"
-                    />
+                    <img :src="item.image" style="width: 80px; height: 80px" />
                   </template>
                   <template #item-btn-function="item">
                     <!-- #item-btn-function="item"  item: valua of row-->
@@ -432,15 +430,33 @@ export default {
     ToolbarRight,
     NotficationClient,
     PopupNotify,
-    ActionLoading
+    ActionLoading,
   },
   data() {
     return {
       isLoading: false,
+      isValid: 0,
       filterTime: "label",
       filterTimeDS: "label",
       classFilterTimeAbout: "d-none",
       classFilterTimeAboutDS: "d-none",
+      msgValidationFor: {
+        warehouse: {
+          name: "",
+          address: "",
+          phone: "",
+        },
+        product: {
+          name: "",
+          sku: "",
+          basePrice: "",
+          publicPrice: "",
+          weight: "",
+          qty: "",
+          image: "",
+          description: "",
+        },
+      },
       headersWarehouse: [
         { text: "Tên kho", value: "name", sortable: true },
         { text: "Địa chỉ", value: "address" },
@@ -470,7 +486,7 @@ export default {
         basePrice: "",
         publicPrice: "",
         description: "",
-        image: ""
+        image: "",
       },
       productImg: "",
       urlImgProductUpload: "",
@@ -565,63 +581,71 @@ export default {
   methods: {
     //warehouse
     createNewWarehouse: function () {
-      this.isLoading = true;
-      axios
-        .post(
-          commonFunction.DOMAIN_URL + "v1/warehouse/save",
-          {
-            customerId: this.idRequest,
-            name: this.formDataWarehouse.name,
-            address: this.formDataWarehouse.address,
-            phone: this.formDataWarehouse.phone,
-          },
-          this.configRequestApi
-        )
-        .then((response) => {
-          if (response.status == 200) {
+      this.isValid = 0;
+      this.validationFormWarehouse();
+      if (this.isValid == 0) {
+        this.isLoading = true;
+        axios
+          .post(
+            commonFunction.DOMAIN_URL + "v1/warehouse/save",
+            {
+              customerId: this.idRequest,
+              name: this.formDataWarehouse.name,
+              address: this.formDataWarehouse.address,
+              phone: this.formDataWarehouse.phone,
+            },
+            this.configRequestApi
+          )
+          .then((response) => {
+            if (response.status == 200) {
+              this.isLoading = false;
+              alert("SUCCESS: Tạo mới thành công - " + response.data.message);
+              commonFunction.reloadPage();
+            } else {
+              this.isLoading = false;
+              alert("FAIL: Đăng kí không thành công! Vui lòng thử lại!");
+            }
+          })
+          .catch((e) => {
             this.isLoading = false;
-            alert("SUCCESS: Tạo mới thành công - " + response.data.message);
-            commonFunction.reloadPage();
-          } else {
-            this.isLoading = false;
-            alert("FAIL: Đăng kí không thành công! Vui lòng thử lại!");
-          }
-        })
-        .catch((e) => {
-          this.isLoading = false;
-          alert("ERROR: Vui lòng thử lại hoặc liên hệ với quản trị viên!");
-          console.log(e);
-        });
+            alert("ERROR: Vui lòng thử lại hoặc liên hệ với quản trị viên!");
+            console.log(e);
+          });
+      }
     },
     updateWarehouse: function () {
-      this.isLoading = true;
-      axios
-        .post(
-          commonFunction.DOMAIN_URL + "v1/warehouse/save",
-          {
-            id: this.formDataWarehouse.id,
-            customerId: this.idRequest,
-            name: this.formDataWarehouse.name,
-            address: this.formDataWarehouse.address,
-            phone: this.formDataWarehouse.phone,
-          },
-          this.configRequestApi
-        )
-        .then((response) => {
-          if (response.status == 200) {
+      this.isValid = 0;
+      this.validationFormWarehouse();
+      if (this.isValid == 0) {
+        this.isLoading = true;
+        axios
+          .post(
+            commonFunction.DOMAIN_URL + "v1/warehouse/save",
+            {
+              id: this.formDataWarehouse.id,
+              customerId: this.idRequest,
+              name: this.formDataWarehouse.name,
+              address: this.formDataWarehouse.address,
+              phone: this.formDataWarehouse.phone,
+            },
+            this.configRequestApi
+          )
+          .then((response) => {
+            if (response.status == 200) {
+              this.isLoading = false;
+              alert("SUCCESS: Cập nhật thành công - " + response.data.message);
+              commonFunction.reloadPage();
+            } else {
+              this.isLoading = false;
+              alert("FAIL: Cập nhật không thành công! Vui lòng thử lại!");
+            }
+          })
+          .catch((e) => {
             this.isLoading = false;
-            alert("SUCCESS: Cập nhật thành công - " + response.data.message);
-            commonFunction.reloadPage();
-          } else {
-            this.isLoading = false;
-            alert("FAIL: Cập nhật không thành công! Vui lòng thử lại!");
-          }
-        })
-        .catch((e) => {
-          this.isLoading = false;
-          alert("ERROR: Vui lòng thử lại hoặc liên hệ với quản trị viên!");
-          console.log(e);
-        });
+            alert("ERROR: Vui lòng thử lại hoặc liên hệ với quản trị viên!");
+            console.log(e);
+          });
+      }
     },
     selectWareHouseForUpdate: function (item) {
       this.formDataWarehouse = {
@@ -667,78 +691,86 @@ export default {
 
     //product
     createNewProduct: function () {
-      this.isLoading = true;
-      let accesstoken_cookies = this.cookies.get("accesstoken_cookies");
-      let formData = new FormData();
-      formData.append("file", this.productImg);
-      // formData.append("id", this.formDataProduct.id);
-      formData.append("customerId", this.idRequest);
-      formData.append("sku", this.formDataProduct.sku);
-      formData.append("qty", this.formDataProduct.qty);
-      formData.append("name", this.formDataProduct.name);
-      formData.append("weight", this.formDataProduct.weight);
-      formData.append("basePrice", this.formDataProduct.basePrice);
-      formData.append("publicPrice", this.formDataProduct.publicPrice);
-      formData.append("description", this.formDataProduct.description);
-      axios
-        .post(commonFunction.DOMAIN_URL + "v1/product/save", formData, {
-          headers: {
-            Authorization: "Bearer " + accesstoken_cookies,
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then((response) => {
-          if (response.status == 200) {
+      this.isValid = 0;
+      this.validationFormProduct();
+      if (this.isValid == 0) {
+        this.isLoading = true;
+        let accesstoken_cookies = this.cookies.get("accesstoken_cookies");
+        let formData = new FormData();
+        formData.append("file", this.productImg);
+        // formData.append("id", this.formDataProduct.id);
+        formData.append("customerId", this.idRequest);
+        formData.append("sku", this.formDataProduct.sku);
+        formData.append("qty", this.formDataProduct.qty);
+        formData.append("name", this.formDataProduct.name);
+        formData.append("weight", this.formDataProduct.weight);
+        formData.append("basePrice", this.formDataProduct.basePrice);
+        formData.append("publicPrice", this.formDataProduct.publicPrice);
+        formData.append("description", this.formDataProduct.description);
+        axios
+          .post(commonFunction.DOMAIN_URL + "v1/product/save", formData, {
+            headers: {
+              Authorization: "Bearer " + accesstoken_cookies,
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((response) => {
+            if (response.status == 200) {
+              this.isLoading = false;
+              alert("SUCCESS: Tạo mới thành công - " + response.data.message);
+              commonFunction.reloadPage();
+            } else {
+              this.isLoading = false;
+              alert("FAIL: Đăng kí không thành công! Vui lòng thử lại!");
+            }
+          })
+          .catch((e) => {
             this.isLoading = false;
-            alert("SUCCESS: Tạo mới thành công - " + response.data.message);
-            commonFunction.reloadPage();
-          } else {
-            this.isLoading = false;
-            alert("FAIL: Đăng kí không thành công! Vui lòng thử lại!");
-          }
-        })
-        .catch((e) => {
-          this.isLoading = false;
-          alert("ERROR: Vui lòng thử lại hoặc liên hệ với quản trị viên!");
-          console.log(e);
-        });
+            alert("ERROR: Vui lòng thử lại hoặc liên hệ với quản trị viên!");
+            console.log(e);
+          });
+      }
     },
     updateProduct: function () {
-      this.isLoading = true;
-      let accesstoken_cookies = this.cookies.get("accesstoken_cookies");
-      let formData = new FormData();
-      formData.append("file", this.productImg);
-      formData.append("id", this.formDataProduct.id);
-      formData.append("customerId", this.idRequest);
-      formData.append("sku", this.formDataProduct.sku);
-      formData.append("qty", this.formDataProduct.qty);
-      formData.append("name", this.formDataProduct.name);
-      formData.append("weight", this.formDataProduct.weight);
-      formData.append("basePrice", this.formDataProduct.basePrice);
-      formData.append("publicPrice", this.formDataProduct.publicPrice);
-      formData.append("description", this.formDataProduct.description);
-      axios
-        .post(commonFunction.DOMAIN_URL + "v1/product/save", formData, {
-          headers: {
-            Authorization: "Bearer " + accesstoken_cookies,
-            "Content-Type": "multipart/form-data",
-          },
-        })
-        .then((response) => {
-          if (response.status == 200) {
+      this.isValid = 0;
+      this.validationFormProduct();
+      if (this.isValid == 0) {
+        this.isLoading = true;
+        let accesstoken_cookies = this.cookies.get("accesstoken_cookies");
+        let formData = new FormData();
+        formData.append("file", this.productImg);
+        formData.append("id", this.formDataProduct.id);
+        formData.append("customerId", this.idRequest);
+        formData.append("sku", this.formDataProduct.sku);
+        formData.append("qty", this.formDataProduct.qty);
+        formData.append("name", this.formDataProduct.name);
+        formData.append("weight", this.formDataProduct.weight);
+        formData.append("basePrice", this.formDataProduct.basePrice);
+        formData.append("publicPrice", this.formDataProduct.publicPrice);
+        formData.append("description", this.formDataProduct.description);
+        axios
+          .post(commonFunction.DOMAIN_URL + "v1/product/save", formData, {
+            headers: {
+              Authorization: "Bearer " + accesstoken_cookies,
+              "Content-Type": "multipart/form-data",
+            },
+          })
+          .then((response) => {
+            if (response.status == 200) {
+              this.isLoading = false;
+              alert("SUCCESS: Cập nhật thành công - " + response.data.message);
+              commonFunction.reloadPage();
+            } else {
+              this.isLoading = false;
+              alert("FAIL: Cập nhật không thành công! Vui lòng thử lại!");
+            }
+          })
+          .catch((e) => {
             this.isLoading = false;
-            alert("SUCCESS: Cập nhật thành công - " + response.data.message);
-            commonFunction.reloadPage();
-          } else {
-            this.isLoading = false;
-            alert("FAIL: Cập nhật không thành công! Vui lòng thử lại!");
-          }
-        })
-        .catch((e) => {
-          this.isLoading = false;
-          alert("ERROR: Vui lòng thử lại hoặc liên hệ với quản trị viên!");
-          console.log(e);
-        });
+            alert("ERROR: Vui lòng thử lại hoặc liên hệ với quản trị viên!");
+            console.log(e);
+          });
+      }
     },
     selectProductForUpdate: function (item) {
       this.formDataProduct = {
@@ -782,6 +814,52 @@ export default {
       this.productImg = this.$refs.productImgUpload.files[0];
       this.urlImgProductUpload = URL.createObjectURL(this.productImg);
       this.loadingImgUpload = false;
+    },
+    validationFormWarehouse() {
+      if (this.formDataWarehouse.name == "") {
+        this.isValid += 1;
+        this.msgValidationFor.warehouse.name =
+          "Vui lòng nhập thông tin kho/cửa hàng!";
+      }
+      if (this.formDataWarehouse.phone == "") {
+        this.isValid += 1;
+        this.msgValidationFor.warehouse.phone =
+          "Vui lòng nhập thông tin liên hệ!";
+      }
+      if (this.formDataWarehouse.address == "") {
+        this.isValid += 1;
+        this.msgValidationFor.warehouse.address =
+          "Vui lòng nhập địa chỉ cụ thể: Số nhà/thôn, xã/phường, quận/huyện, tỉnh/thành phố!";
+      }
+    },
+    validationFormProduct() {
+      if (this.formDataProduct.name == "") {
+        this.isValid += 1;
+        this.msgValidationFor.product.name = "Vui lòng nhập tên sản phẩm!";
+      }
+
+      if (this.formDataProduct.sku == "") {
+        this.isValid += 1;
+        this.msgValidationFor.product.sku = "Vui lòng nhập mã SKU!";
+      }
+
+      if (this.formDataProduct.basePrice == "") {
+        this.isValid += 1;
+        this.msgValidationFor.product.basePrice =
+          "Vui lòng nhập giá gốc của sản phẩm!";
+      }
+
+      if (this.formDataProduct.publicPrice == "") {
+        this.isValid += 1;
+        this.msgValidationFor.product.publicPrice =
+          "Vui lòng nhập giá bán của sản phẩm!";
+      }
+
+      if (this.$refs.productImgUpload.files[0] == null) {
+        this.isValid += 1;
+        this.msgValidationFor.product.image =
+          "Vui lòng upload ảnh cho sản phẩm!";
+      }
     },
   },
 };
