83ec911b1985e010e85f0a46015f4ba58a6d9218
DuongPH <chieuduong1102@gmail.com>
2022-11-12 17:02:15 +0700

    QT-48 Management Customer Admin

--- views/src/components/admin/customer/ManagementCustomerAdmin.vue
+++ views/src/components/admin/customer/ManagementCustomerAdmin.vue
@@ -6,9 +6,39 @@
                 <h4 class="title-management">
                     Danh sách khách hàng
                 </h4>
+                <span><b>Tổng số: </b> 1000</span>
                 <hr>
+                <div class="form-filter-customer">
+                    <div class="row">
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" placeholder="Mã Khách hàng">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" placeholder="Tên Khách hàng">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" placeholder="Tên Công ty">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="col-md-2">
+                            <input type="text" class="form-control" placeholder="Số điện thoại">
+                        </div>
+                        <div class="col-md-2">
+                            <button class="btn btn-danger me-1">Tìm kiếm</button> <button class="btn btn-secondary">Reset</button>
+                        </div>
+                    </div>
+                </div>
+                <hr>
+                <div class="table-list-customer">
+                    <easy-data-table :headers="hearderCustomer" :items="customerList"
+                        table-class-name="easy-data-table-customize">
+                    </easy-data-table>
+                    <br>
+                </div>
             </div>
-            
+
         </div>
     </div>
 </template>
@@ -34,6 +64,21 @@
 
         data() {
             return {
+                customerList: [],
+                hearderCustomer: [
+                    { text: "Mã KH", value: "customerId", sortable: true },
+                    { text: "Tên Khách hàng", value: "fullName", sortable: true },
+                    { text: "Shop/Cửa hàng/Công ty", value: "companyName", sortable: true },
+                    { text: "SĐT", value: "phone" },
+                    { text: "Email", value: "email"},
+                    { text: "Đường", value: "email"},
+                    { text: "Xã/Phường/Thị trấn", value: "ward"},
+                    { text: "Quận/Huyện", value: "district"},
+                    { text: "Tỉnh/Thành phố", value: "province"},
+                    { text: "Ngày tạo", value: "createdAt"},
+                    { text: "Cập nhật", value: "updatedAt"},
+                    { text: "Chức năng", value: "btn-function-order" },
+                ],
             };
         },
 
