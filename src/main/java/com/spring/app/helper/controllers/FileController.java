e21e8f3fc7eadf4c3ef7405b1d3f5203d0758ada
Kat <katforscript@gmail.com>
2022-11-14 11:47:18 +0700

    QT-28:Customer cid image upload service

--- src/main/java/com/spring/app/helper/controllers/FileController.java
+++ src/main/java/com/spring/app/helper/controllers/FileController.java
@@ -1,6 +1,8 @@
 package com.spring.app.helper.controllers;
 
+import com.spring.app.customers.service.CustomerService;
 import com.spring.app.helper.services.FilesStorageServiceImpl;
+import com.spring.app.products.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -8,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.File;
 import java.io.IOException;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -16,10 +19,14 @@ import java.io.IOException;
 public class FileController {
     @Autowired
     private FilesStorageServiceImpl service;
+    @Autowired
+    private ProductService productService;
+    @Autowired
+    private CustomerService customerService;
 
     @GetMapping("/product/{path}/{fileName}")
-    public ResponseEntity<?> downloadImageFromFileSystem(@Valid @PathVariable String path, @PathVariable String fileName) throws IOException {
-        service.setPath(path);
+    public ResponseEntity<?> productImageByCustomer(@Valid @PathVariable String path, @PathVariable String fileName) throws IOException {
+        service.setPath(productService.getImageScope(path));
         byte[] imageData = service.downloadImageFromFileSystem(fileName);
 
         return ResponseEntity.status(HttpStatus.OK)
@@ -28,9 +35,19 @@ public class FileController {
     }
 
     @GetMapping("/product/{fileName}")
-    public ResponseEntity<?> downloadImageFromFileSystem(@Valid @PathVariable String fileName) throws IOException {
-        service.clearPath();
+    public ResponseEntity<?> productImage(@Valid @PathVariable String fileName) throws IOException {
+        service.setPath(ProductService.SCOPE);
         byte[] imageData = service.downloadImageFromFileSystem(fileName);
+        return ResponseEntity.status(HttpStatus.OK)
+                .contentType(MediaType.valueOf("image/png"))
+                .body(imageData);
+    }
+
+    @GetMapping("/customer/{path}/{fileName}")
+    public ResponseEntity<?> getCustomerImage(@Valid @PathVariable String path, @PathVariable String fileName) throws IOException {
+        service.setPath(customerService.getImageScope(path));
+        byte[] imageData = service.downloadImageFromFileSystem(fileName);
+
         return ResponseEntity.status(HttpStatus.OK)
                 .contentType(MediaType.valueOf("image/png"))
                 .body(imageData);
