e7a0597f0544d59f8048db3ce080914a66825d57
Kat <katforscript@gmail.com>
2022-11-14 10:15:48 +0700

    QT-28:Customer change password feature

--- src/main/java/com/spring/app/authentication/controllers/AuthController.java
+++ src/main/java/com/spring/app/authentication/controllers/AuthController.java
@@ -8,10 +8,10 @@ import javax.validation.Valid;
 
 import com.spring.app.authentication.models.User;
 import com.spring.app.authentication.payload.request.LoginRequest;
+import com.spring.app.authentication.payload.request.ResetPasswordRequest;
 import com.spring.app.authentication.payload.response.JwtResponse;
 import com.spring.app.authentication.payload.request.ForgotPasswordRequest;
 import com.spring.app.customers.payload.response.ForgotPasswordResponse;
-import com.spring.app.email.models.Mail;
 import com.spring.app.email.service.MailServiceImp;
 import com.spring.app.payload.MessageResponse;
 import com.spring.app.authentication.models.repository.UserRepository;
@@ -23,6 +23,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -36,11 +37,10 @@ public class AuthController {
     @Autowired
     JwtUtils jwtUtils;
     @Autowired
-    MailServiceImp mailServiceImp;
+    PasswordEncoder encoder;
 
     @PostMapping("/login")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
-
         Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
 
@@ -57,6 +57,25 @@ public class AuthController {
                 userDetails.getUsername(),
                 userDetails.getEmail(),
                 roles));
+    }
+
+    @PostMapping("/reset/password")
+    public ResponseEntity<?> resetPassword(@Valid @RequestBody ResetPasswordRequest resetPassword) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(resetPassword.getUsername(), resetPassword.getPassword()));
+
+        if (authentication.isAuthenticated()) {
+            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+            User user = userRepository.findById(userDetails.getId())
+                    .orElseThrow(() -> new RuntimeException("User not found!"));
+
+            user.setPassword(encoder.encode(resetPassword.getPassword()));
+            userRepository.save(user);
+
+            return ResponseEntity.ok(new MessageResponse("Update password success"));
+        }
+
+        return ResponseEntity.badRequest().body(new MessageResponse("Can not reset password!"));
     }
 
     // update password function //
