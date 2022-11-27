039770ac51d6ed6d31a83286d1892d3443065d22
Kat <katforscript@gmail.com>
2022-11-28 00:52:33 +0700

    QT-56:After save order done

--- src/main/java/com/spring/app/orders/event/OrderListener.java
+++ src/main/java/com/spring/app/orders/event/OrderListener.java
@@ -2,6 +2,7 @@ package com.spring.app.orders.event;
 
 import com.spring.app.liquidity.service.LiquidityService;
 import com.spring.app.orders.models.Order;
+import com.spring.app.util.BeanUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,14 +12,15 @@ import javax.persistence.PostUpdate;
 
 @Component
 public class OrderListener {
-
-    @Autowired
-    LiquidityService liquidityService;
-
     @PostPersist
     @PostUpdate
     @PostRemove
     private void afterAnyUpdate(Order order) {
-        liquidityService.processLiquidity(order);
+        try {
+            LiquidityService liquidityService = BeanUtil.getBean(LiquidityService.class);
+            liquidityService.processLiquidity(order);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
